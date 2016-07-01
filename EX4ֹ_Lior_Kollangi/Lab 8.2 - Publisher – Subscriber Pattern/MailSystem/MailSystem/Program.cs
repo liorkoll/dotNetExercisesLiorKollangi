394def486fb5da249f29bd8c047095cbb52378a2
liorkoll <liorkoll@gmail.com>
2016-07-01 09:35:21 +0100

    add program

---
+++ EX4ֹ_Lior_Kollangi/Lab 8.2 - Publisher – Subscriber Pattern/MailSystem/MailSystem/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MailSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MailManager mailManager = new MailManager();
+            mailManager.MailArrived += Mm_MailArrived;
+            mailManager.SimulateMailArrived();
+            Timer timer = new Timer(new TimerCallback(TimerCallback), mailManager, 1000, 1000);
+            Thread.Sleep(10000);
+        }
+        private static void TimerCallback(object state)
+        {
+            var mailManager = state as MailManager;
+            mailManager.SimulateMailArrived();
+        }
+        static void Mm_MailArrived(object sender, MailArrivedEventArgs e)
+        {
+            Console.WriteLine("Title: " + e.Title);
+            Console.WriteLine("Body: " + e.Body);
+        }
+    }
+}
