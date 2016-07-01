73289563ee869edf90823a395d8af9ec95f7cdb9
liorkoll <liorkoll@gmail.com>
2016-07-01 09:26:36 +0100

    add program.cs main

--- EX4ֹ_Lior_Kollangi/Lab 8.1 - Using Delegates/CustomersApp/CustomersApp/Customer.cs
+++ EX4ֹ_Lior_Kollangi/Lab 8.1 - Using Delegates/CustomersApp/CustomersApp/Customer.cs
@@ -11,13 +11,13 @@ namespace CustomersApp
 
     class Customer : IComparable<Customer>, IEquatable<Customer>
     {
-        public string Name { get; }
-        public int ID { get; }
-        public string Address { get; }
-        public Customer(string Name, int ID,string Address){
-            this.ID = ID;
-            this.Name = Name;
-            this.Address = Address;
+        public string Name { get; private set; }
+        public int ID { get;private set; }
+        public string Address { get; private set; }
+        public Customer(string name, int id,string address){
+            this.ID = id;
+            this.Name = name;
+            this.Address = address;
             }
 
 
@@ -32,66 +32,14 @@ namespace CustomersApp
             return this.CompareTo(other) == 0 && this.ID == other.ID;
             
         }
-        public static ICollection<Customer> GetCustomers(ICollection<Customer> customers,CustomerFilter customerFilter)
-        {
-            List<Customer> filterListCustomers = new List<Customer>();
-            if (customerFilter != null)
-            {
-                foreach (Customer customer in customers)
-                {
-                    if (customerFilter(customer))
-                    {
-                        filterListCustomers.Add(customer);
-                    }
-                }
-            }
-            return filterListCustomers;
-        }
-        public static bool startWithAK(Customer customer)
-        {
-            return (customer.Name.ElementAt(0) >= 'A' && customer.Name.ElementAt(0) <= 'K');                               
-
-        }
+       
+      
 
 
 
 
 
-        static void Main(string[] args)
-        {
-            
-                ICollection<Customer> listCustomers = new List<Customer>();
-                listCustomers.Add(new Customer("AVI", 123, "KK 1"));
-                listCustomers.Add(new Customer("MAOR", 99, "KK 1"));
-                listCustomers.Add(new Customer("DAVID", 125, "KK 1"));
-           
-            ICollection<Customer> listCustomers1 = new List<Customer>();
-            ICollection<Customer> listCustomers2 = new List<Customer>();
-            ICollection<Customer> listCustomers3 = new List<Customer>();
-
-
-            listCustomers1 = GetCustomers(listCustomers, new CustomerFilter(startWithAK));
-
-            foreach (Customer c in listCustomers1)
-            {
-                Console.WriteLine(c.Name);
-            }
-            listCustomers2 = GetCustomers(listCustomers, delegate (Customer customer)
-            {
-                return (customer.Name.ElementAt(0) >= 'L' && customer.Name.ElementAt(0) <= 'Z');
-            });
-            foreach (Customer c in listCustomers2)
-            {
-                Console.WriteLine(c.Name);
-            }
-            listCustomers3 = GetCustomers(listCustomers, customer =>
-            {             
-                return (customer.ID<100);
-        });
-            foreach (Customer c in listCustomers3)
-            {
-                Console.WriteLine(c.ID);
-            }
-        }
+       
+        
     }
 }
---
+++ EX4ֹ_Lior_Kollangi/Lab 8.1 - Using Delegates/CustomersApp/CustomersApp/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomersApp
+{
+    class Program
+    {
+        public static ICollection<Customer> GetCustomers(ICollection<Customer> customers, CustomerFilter customerFilter)
+        {
+            List<Customer> filterListCustomers = new List<Customer>();
+            if (customerFilter != null)
+            {
+                foreach (Customer customer in customers)
+                {
+                    if (customerFilter(customer))
+                    {
+                        filterListCustomers.Add(customer);
+                    }
+                }
+            }
+            return filterListCustomers;
+        }
+        public static bool startWithAK(Customer customer)
+        {
+            return (customer.Name.ElementAt(0) >= 'A' && customer.Name.ElementAt(0) <= 'K');
+
+        }
+        static void Main(string[] args)
+        {
+
+            ICollection<Customer> listCustomers = new List<Customer>();
+            listCustomers.Add(new Customer("AVI", 123, "KK 1"));
+            listCustomers.Add(new Customer("MAOR", 99, "KK 1"));
+            listCustomers.Add(new Customer("DAVID", 125, "KK 1"));
+
+            ICollection<Customer> listCustomers1 = new List<Customer>();
+            ICollection<Customer> listCustomers2 = new List<Customer>();
+            ICollection<Customer> listCustomers3 = new List<Customer>();
+
+
+            listCustomers1 = GetCustomers(listCustomers, new CustomerFilter(startWithAK));
+
+            foreach (Customer c in listCustomers1)
+            {
+                Console.WriteLine(c.Name);
+            }
+            listCustomers2 = GetCustomers(listCustomers, delegate(Customer customer)
+            {
+                return (customer.Name.ElementAt(0) >= 'L' && customer.Name.ElementAt(0) <= 'Z');
+            });
+            foreach (Customer c in listCustomers2)
+            {
+                Console.WriteLine(c.Name);
+            }
+            //    listCustomers3 = GetCustomers(listCustomers, customer =>
+            //    {             
+            //        return (customer.ID<100);
+            //});
+
+            var list = GetCustomers(listCustomers, c => c.ID < 100);
+            foreach (Customer c in listCustomers3)
+            {
+                Console.WriteLine(c.ID);
+            }
+        }
+    }
+
+}
