f3d8f04b6df06796c35093da4b0bd74bcd662bb9
Liorkoll <liorkoll@gmail.com>
2016-07-07 20:15:41 +0300

    add ex6 - Lab 1.1 Dynamic Invocation

---
+++ EX6_Lior_Kollangi/DynInvoke/DynInvoke/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynInvoke
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A a = new A();
+            B b = new B();
+            C c = new C();
+            Console.WriteLine(InvokeHello(a,"Lior"));
+            Console.WriteLine(InvokeHello(b,"Alexender"));
+            Console.WriteLine(InvokeHello(c, "Stass"));
+
+        }
+        public static string InvokeHello(object o,string str)
+        {
+            MethodInfo methodInfo = o.GetType().GetMethod("Hello",
+                new Type[] { typeof(string) });
+            return (string)methodInfo.Invoke(o, new object[] { str });
+        }
+    }
+}
