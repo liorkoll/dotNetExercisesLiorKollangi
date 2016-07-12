3b0d37696b4566414f390125464c7322c8d1b710
Liorkoll <liorkoll@gmail.com>
2016-07-12 22:08:13 +0300

    last

--- BackgammonProject/Backgammon/BackgammonConsole/Program.cs
+++ BackgammonProject/Backgammon/BackgammonConsole/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BackgammonLogic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,86 @@ namespace BackgammonConsole
     {
         static void Main(string[] args)
         {
+            Board b = new Board();
+            Console.WriteLine(b.Cells[6 + 12].CheckersColor == CheckerColor.Red);
+
+
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine();
+                for (int j = 0; j < 12; j++)
+                {
+
+                    if ((b.Cells[j + 12].CheckersColor == CheckerColor.Black && (b.Cells[j + 12].NumOfCheckers > 5 - i || b.Cells[j + 12].NumOfCheckers>=5)))
+                    {
+                         Console.Write("|B|");
+
+                    }
+                    else if ((b.Cells[j + 12].CheckersColor == CheckerColor.Red && (b.Cells[j + 12].NumOfCheckers > 5 - i || b.Cells[j + 12].NumOfCheckers >= 5)))
+                        {
+                        Console.Write("|R|");
+                       
+                    }
+                    else
+                    {
+                        Console.Write("| |");
+                    }
+                                      
+                }
+            }
+            Console.WriteLine();
+            int x = 11;
+
+            for (int i = 0; i < 6; i++)
+            {               
+                Console.WriteLine();
+                //Console.WriteLine("| |");
+                for (int j = 0; j < 12; j++)                   
+                {
+                    if (j == 0)
+                    {
+                        x = 11;
+                    }
+                    if ((b.Cells[j + x].CheckersColor == CheckerColor.Black && (b.Cells[j + x].NumOfCheckers > 5 - i || b.Cells[j + 12].NumOfCheckers >= 5)))
+                    {
+                        Console.Write("|B|");                      
+                    }
+                    else if ((b.Cells[j + x].CheckersColor == CheckerColor.Red  && (b.Cells[j + x].NumOfCheckers > 5 - i || b.Cells[j + 12].NumOfCheckers >= 5)))
+                    {
+                        Console.Write("|R|");                       
+                        
+                    }
+                    else
+                    {
+                        Console.Write("| |");
+                    }                                  
+                        x = x - 2;
+                   
+                }                
+            }                                           
+            }
+        public static String drawCheckerX(Board b,int i,int j)
+        {
+            string checker = "| |";
+            int count = b.Cells[j+12].NumOfCheckers;
+            if (5 - count >= i)
+            {
+                checker = "|X|";
+            }
+            return checker;
         }
+        public static String drawCheckerO(Board b, int i, int j)
+        {
+            string checker = "| |";
+            int count = b.Cells[j + 12].NumOfCheckers;
+            if  (count <= i-5)
+            {
+                checker = "|O|";
+            }
+            return checker;
+        }
+
+
     }
-}
+    }
---
+++ EX5_Lior_Kollangi/Lab 10.1 - Accessing Files/Personnel/Personnel/Personnel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personnel
+{
+    public class Personnel
+    {
+        public List<string> ReadFile(string path)
+        {
+            StreamReader streamReader = new StreamReader(path);
+            List<string> lines = new List<string>();
+            string line;
+            while ((line = streamReader.ReadLine()) != null)
+            {
+                lines.Add(line);
+            }
+            return lines;
+        }
+    }
+}
