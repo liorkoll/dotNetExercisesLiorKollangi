9dff157b1b40a78c0b6a4aeb30c6f4395a4e8801
Liorkoll <liorkoll@gmail.com>
2016-07-22 16:34:51 +0300

    threads

--- BackgammonProject/Backgammon/BackgammonLogic/Player.cs
+++ BackgammonProject/Backgammon/BackgammonLogic/Player.cs
@@ -6,16 +6,19 @@ using System.Threading.Tasks;
 
 namespace BackgammonLogic
 {
-    public abstract class Player
+    public interface  Player
     {
-        public bool Turn { get; set; }
-        public CheckerColor PlayerColor { get; private set; }
-        public string Name { get; private set; }
-        public Player(string name, CheckerColor color)
-        {
-            Name = name;
-            PlayerColor = color;
-        }
+        //public bool Turn { get; set; }
+        //public CheckerColor PlayerColor { get; private set; }
+        //public string Name { get; private set; }
+        //public Player(string name, CheckerColor color)
+        //{
+        //    Name = name;
+        //    PlayerColor = color;
+        //}
+
+
+
 
     }
 }
