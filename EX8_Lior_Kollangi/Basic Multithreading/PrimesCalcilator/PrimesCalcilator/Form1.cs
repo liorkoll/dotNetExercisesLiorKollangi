174de464b684ca030e03edab5a491acbc117afe0
liorkoll <liorkoll@gmail.com>
2016-07-22 12:05:49 +0300

    start cancel button

--- EX8_Lior_Kollangi/Basic Multithreading/PrimesCalcilator/PrimesCalcilator/Form1.cs
+++ EX8_Lior_Kollangi/Basic Multithreading/PrimesCalcilator/PrimesCalcilator/Form1.cs
@@ -13,6 +13,10 @@ namespace PrimesCalcilator
 {
     public partial class Form1 : Form
     {
+       public static CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+       public static CancellationToken cancellationToken = cancellationTokenSource.Token;
+
+
         public Form1()
         {
             InitializeComponent();
@@ -23,24 +27,34 @@ namespace PrimesCalcilator
             
            PrimesCalc pc = new PrimesCalc();
            var synchronizationContext = SynchronizationContext.Current;
-          
 
-         
+
+           if (cancellationToken != null) cancellationTokenSource = new CancellationTokenSource();
            Task.Run (()=>   
             {
                 int [] primes = pc.CalcPrimes(int.Parse(textBox1.Text),int.Parse(textBox2.Text));
-
+                cancellationToken.ThrowIfCancellationRequested();
+                Task.Delay(10000);
              synchronizationContext.Send(o =>
 
            {
+               
                listBox1.Items.Clear();
                 for (int i = 0; i < primes.Length; i++)
                     listBox1.Items.Add(primes[i].ToString());
           }, null);
         });
 
+         
 
 
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(cancellationToken!=null) cancellationTokenSource.Cancel();
+         
         }
       
     }
