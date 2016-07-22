1fd8ce99f0cc30502ec2d0922e725669eaf3e3f0
liorkoll <liorkoll@gmail.com>
2016-07-22 11:06:59 +0300

    finish basic multithreading ex

---
+++ EX8_Lior_Kollangi/Basic Multithreading/PrimesCalcilator/PrimesCalcilator/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrimesCalcilator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+           PrimesCalc pc = new PrimesCalc();
+           var synchronizationContext = SynchronizationContext.Current;
+          
+
+         
+           Task.Run (()=>   
+            {
+                int [] primes = pc.CalcPrimes(int.Parse(textBox1.Text),int.Parse(textBox2.Text));
+
+             synchronizationContext.Send(o =>
+
+           {
+               listBox1.Items.Clear();
+                for (int i = 0; i < primes.Length; i++)
+                    listBox1.Items.Add(primes[i].ToString());
+          }, null);
+        });
+
+
+
+        }
+      
+    }
+}
