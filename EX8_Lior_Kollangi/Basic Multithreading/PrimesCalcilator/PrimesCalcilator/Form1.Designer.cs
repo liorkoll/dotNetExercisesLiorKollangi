f615ec44f268916e76f64e84640d296fb8c80aa3
liorkoll <liorkoll@gmail.com>
2016-07-29 08:29:47 +0100

    finish CancellationToken

--- EX8_Lior_Kollangi/Basic Multithreading/PrimesCalcilator/PrimesCalcilator/Form1.Designer.cs
+++ EX8_Lior_Kollangi/Basic Multithreading/PrimesCalcilator/PrimesCalcilator/Form1.Designer.cs
@@ -80,7 +80,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(200, 72);
+            this.button3.Location = new System.Drawing.Point(184, 72);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 5;
--- EX8_Lior_Kollangi/Basic Multithreading/PrimesCalcilator/PrimesCalcilator/PrimesCalc.cs
+++ EX8_Lior_Kollangi/Basic Multithreading/PrimesCalcilator/PrimesCalcilator/PrimesCalc.cs
@@ -3,21 +3,32 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace PrimesCalcilator
 {
     class PrimesCalc
     {
-        public int[] CalcPrimes(int x, int y)
+        public ManualResetEvent waitHandle {get;set;}//true
+
+        public PrimesCalc()
+        {
+            waitHandle = new ManualResetEvent(true);
+        }
+        public int[] CalcPrimes(int x, int y, CancellationToken cancellationToken)
         {
             int count = 0;
             ArrayList list = new ArrayList();
 
             for (int i = x; i < y; i++)
             {
+                Thread.Sleep(100);
                 for (int j = 1; j < y; j++)
                 {
+                  
+                   
                     if (i % j == 0)
                     {
                         count++;
@@ -26,6 +37,15 @@ namespace PrimesCalcilator
                 if (count == 2)
                 {
                     list.Add(i);
+                }
+                if (!waitHandle.WaitOne(0))
+                {
+                   
+                    break;
+                }
+                if (cancellationToken.IsCancellationRequested)
+                {
+                    break;
                 }
                 count = 0;
             }
