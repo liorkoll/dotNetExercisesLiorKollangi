0ec73b67aa78fe392438a27650ee2a6657fa573a
Liorkoll <liorkoll@gmail.com>
2016-08-05 00:03:53 +0300

    backgammon

---
+++ EX8_Lior_Kollangi/Inter-process-Syncronization/SyncDemo/SyncDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SyncDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mutex mutex = new Mutex(false, "SyncFileMutex");
+             for (int i = 0; i < 10000; i++)
+            {
+               mutex.WaitOne();
+                StreamWriter writer = new StreamWriter(@"data1.txt", true);
+                writer.WriteLine("Writing to Process " + Process.GetCurrentProcess().Id);
+                Console.WriteLine("Process ID: {0}", Process.GetCurrentProcess().Id);
+                writer.Close();
+                mutex.ReleaseMutex();
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
+    
