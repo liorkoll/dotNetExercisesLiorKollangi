86147719b576b41fe35bc5cdc31860cb3308113e
liorkoll <liorkoll@gmail.com>
2016-08-04 13:17:02 +0300

    merge

---
+++ EX8_Lior_Kollangi/Inter-process-Syncronization/SyncDemo/SyncDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SyncDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mutex mutex = new Mutex(false, "SyncFileMutex");
+            StreamWriter writer = new StreamWriter(@"data.txt",true);
+            for(int i = 0; i < 1000; i++)
+            {
+                mutex.WaitOne();
+                writer.Write("Proceess Id:{0}",Process.GetCurrentProcess().Id);
+                mutex.ReleaseMutex();
+            }
+            Console.ReadLine();
+        }
+    }
+}
