c61585ee53fd59d6823c0ceb5d1718aeecc74c5a
liorkoll <liorkoll@gmail.com>
2016-08-12 09:18:40 +0100

    finish hw

--- EX10_Lior_Kollangi/Resource Management Lab/The Dispose Pattern/Job/Job/Program.cs
+++ EX10_Lior_Kollangi/Resource Management Lab/The Dispose Pattern/Job/Job/Program.cs
@@ -10,19 +10,25 @@ namespace Jobs {
     {
         static void Main(string[] args)
         {
-            using (Job job = new Job("MyJob", 1000000)) 
+            var sw = new Stopwatch();
+            sw.Start();
+            using (Job job = new Job("MyJob", 10485760)) 
             {
                 for (int i = 0; i < 20; i++)
                 {
-                    Process p = Process.Start("notepad");
+                    Process p = Process.Start("mspaint");
                     job.AddProcessToJob(p);
 
                 }
-
+               
                 Console.WriteLine("Press enter to kill all notepad processs ");
                 Console.ReadLine();
                 job.Kill();
+               
             }
+            sw.Stop();
+            Console.WriteLine(sw.Elapsed);
+            
         }
     }
 }
